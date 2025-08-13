91cd14e0cc118475bf58508f2b1921f14f00e82f
yassine mansour <yassineaitman@gmail.com>
2025-08-13 11:50:48 +0100

    move require APP from line 4 to 10

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php 
 
 declare(strict_types = 1);
-require (APP_PATH . 'functions.php');
 
 $root = dirname(__FILE__) . DIRECTORY_SEPARATOR;
 define('APP_PATH' , $root . 'src' . DIRECTORY_SEPARATOR);
 define('DATA_PATH' , $root . 'ressources' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH' , $root . 'views' . DIRECTORY_SEPARATOR);
+
+require (APP_PATH . 'functions.php');
 
 $files = getFiles(DATA_PATH);
 
